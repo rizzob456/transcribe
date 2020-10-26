37eeb6014ec3f107b50874392504cc0e0c329347
Sanjay Kumar <ssankm@amazon.com>
2020-10-26 16:14:37 -0400

    first commit

---
+++ src/main/java/transcribe/Constants.java
@@ -0,0 +1,11 @@
+package transcribe;
+
+public final class Constants {
+	
+	public static final String awsAccessKey = "AKIAXPC5JO4EDYOF5JX6";
+	public static final String awsSecretKey = "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";
+	public static final String bucketName = "sanjay-transcribe-new";
+	public static final String senderEmail = "ssankm@amazon.com";
+
+
+}
